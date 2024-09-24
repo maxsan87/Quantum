2b8fb1df78a982b22c5a15c606e1e674db8bec98
maxsan87 <sanchezmaximo229@gmail.com>
2024-09-24 20:54:17 -0300

    Actu 4.3
    Correccion de la navbar
    Decoraciones extras a botones y alertas

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var popup = document.getElementById('popup');
     var cerrarPopup = document.getElementById('cerrar-popup');
 
-    // Mostrar el pop-up al hacer clic en el botón
     mostrarPopup.addEventListener('click', function() {
         popup.style.display = 'flex'; // Mostrar pop-up
     });
 
-    // Cerrar el pop-up al hacer clic en el icono de cerrar
     cerrarPopup.addEventListener('click', function() {
         popup.style.display = 'none'; // Ocultar pop-up
     });
 
-    // Cerrar el pop-up al hacer clic fuera del contenido del pop-up
     popup.addEventListener('click', function(event) {
         if (event.target === popup) {
             popup.style.display = 'none'; // Ocultar pop-up
